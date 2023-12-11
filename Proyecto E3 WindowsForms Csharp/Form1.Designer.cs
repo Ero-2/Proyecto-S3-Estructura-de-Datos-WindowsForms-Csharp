da3fc90907d0a57527e2ccc05140ea0b7d25eb28
Ero-2 <125475081+Ero-2@users.noreply.github.com>
2023-12-10 20:06:23 -0600

    Agregar archivos de proyecto.

---
+++ Proyecto E3 WindowsForms Csharp/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace Proyecto_E3_WindowsForms_Csharp
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.linkedListLabel = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.stackLabel = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.queueLabel = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.treeLabel = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.graphLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(239, 75);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(97, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "LISTA ENLAZADA";
+            // 
+            // linkedListLabel
+            // 
+            this.linkedListLabel.AutoSize = true;
+            this.linkedListLabel.Location = new System.Drawing.Point(382, 75);
+            this.linkedListLabel.Name = "linkedListLabel";
+            this.linkedListLabel.Size = new System.Drawing.Size(0, 13);
+            this.linkedListLabel.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(239, 127);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(33, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "PILA:";
+            // 
+            // stackLabel
+            // 
+            this.stackLabel.AutoSize = true;
+            this.stackLabel.Location = new System.Drawing.Point(382, 127);
+            this.stackLabel.Name = "stackLabel";
+            this.stackLabel.Size = new System.Drawing.Size(0, 13);
+            this.stackLabel.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(237, 174);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "COLA:";
+            // 
+            // queueLabel
+            // 
+            this.queueLabel.AutoSize = true;
+            this.queueLabel.Location = new System.Drawing.Point(385, 173);
+            this.queueLabel.Name = "queueLabel";
+            this.queueLabel.Size = new System.Drawing.Size(0, 13);
+            this.queueLabel.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(240, 224);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(46, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "ARBOL:";
+            // 
+            // treeLabel
+            // 
+            this.treeLabel.AutoSize = true;
+            this.treeLabel.Location = new System.Drawing.Point(385, 223);
+            this.treeLabel.Name = "treeLabel";
+            this.treeLabel.Size = new System.Drawing.Size(0, 13);
+            this.treeLabel.TabIndex = 7;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(240, 275);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(47, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "GRAFO:";
+            // 
+            // graphLabel
+            // 
+            this.graphLabel.AutoSize = true;
+            this.graphLabel.Location = new System.Drawing.Point(385, 274);
+            this.graphLabel.Name = "graphLabel";
+            this.graphLabel.Size = new System.Drawing.Size(0, 13);
+            this.graphLabel.TabIndex = 9;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.graphLabel);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.treeLabel);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.queueLabel);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.stackLabel);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.linkedListLabel);
+            this.Controls.Add(this.label1);
+            this.Name = "MainForm";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.MainForm_Load_1);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label linkedListLabel;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label stackLabel;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label queueLabel;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label treeLabel;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label graphLabel;
+    }
+}
+
---
+++ Proyecto E3 WindowsForms Csharp/Form1.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.TreeView;
+
+namespace Proyecto_E3_WindowsForms_Csharp
+{
+    public partial class MainForm : Form
+    {
+
+        private LinkedList<int> linkedList;
+        private Stack<int> stack;
+        private Queue<int> queue;
+        private TreeNode rootNode;
+        private Graph graph;
+
+        public MainForm()
+        {
+            InitializeComponent();
+            InitializeDataStructures();
+        }
+
+        private void InitializeDataStructures()
+        {
+            // Lista Enlazada
+            linkedList = new LinkedList<int>();
+            linkedList.AddLast(1);
+            linkedList.AddLast(2);
+            linkedList.AddLast(3);
+
+            // Pila
+            stack = new Stack<int>();
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+
+            // Cola
+            queue = new Queue<int>();
+            queue.Enqueue(1);
+            queue.Enqueue(2);
+            queue.Enqueue(3);
+
+            // Árbol Binario
+            rootNode = new TreeNode(1);
+            rootNode.Left = new TreeNode(2);
+            rootNode.Right = new TreeNode(3);
+
+            // Grafo No Dirigido
+            graph = new Graph();
+            graph.AddVertex(1);
+            graph.AddVertex(2);
+            graph.AddVertex(3);
+
+            graph.AddEdge(1, 2);
+            graph.AddEdge(2, 3);
+        }
+
+
+        private void DisplayData()
+        {
+            // Mostrar en la interfaz gráfica
+            linkedListLabel.Text = "Linked List: " + string.Join(" -> ", linkedList);
+            stackLabel.Text = "Stack: " + string.Join(" -> ", stack);
+            queueLabel.Text = "Queue: " + string.Join(" -> ", queue);
+
+            // Árbol Binario (Inorder Traversal)
+            treeLabel.Text = "Binary Tree (Inorder Traversal): ";
+            InorderTraversal(rootNode);
+
+            // Grafo No Dirigido
+            graphLabel.Text = "Graph Representation:\n" + graph.GetGraphString();
+        }
+
+
+        private void InorderTraversal(TreeNode node)
+        {
+            if (node != null)
+            {
+                InorderTraversal(node.Left);
+                treeLabel.Text += $"{node.Value} ";
+                InorderTraversal(node.Right);
+            }
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            DisplayData();
+        }
+
+        public class TreeNode
+        {
+            public int Value;
+            public TreeNode Left;
+            public TreeNode Right;
+
+            public TreeNode(int value)
+            {
+                Value = value;
+            }
+        }
+
+        public class Graph
+        {
+            private Dictionary<int, List<int>> adjacencyList;
+
+            public Graph()
+            {
+                adjacencyList = new Dictionary<int, List<int>>();
+            }
+
+            public void AddVertex(int vertex)
+            {
+                adjacencyList[vertex] = new List<int>();
+            }
+
+            public void AddEdge(int source, int destination)
+            {
+                adjacencyList[source].Add(destination);
+                adjacencyList[destination].Add(source);
+            }
+
+            public string GetGraphString()
+            {
+                string result = "";
+                foreach (var vertex in adjacencyList.Keys)
+                {
+                    result += $"{vertex}: ";
+                    result += string.Join(" ", adjacencyList[vertex]);
+                    result += "\n";
+                }
+                return result;
+            }
+        }
+
+        private void MainForm_Load_1(object sender, EventArgs e)
+        {
+            DisplayData();
+        }
+    }
+
+}
+
